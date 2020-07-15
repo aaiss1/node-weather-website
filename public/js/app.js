fb6f28cb95b790d9ccff5d813d4ac32ef72bcdcc
Anees Aissaoui <31697581+aaiss1@users.noreply.github.com>
2020-07-15 17:32:17 -0400

    App setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-fetch('http://localhost:3000/weather?address=Ottawa').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -21,7 +10,8 @@ weatherForm.addEventListener('submit', (e) => {
     
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent= data.error;
